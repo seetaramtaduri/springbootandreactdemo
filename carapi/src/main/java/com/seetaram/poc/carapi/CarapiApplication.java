993139c0f5b778e04de99983bd8bb3259fda2549
seetaram <seetaram.taduri@gmail.com>
2022-06-22 12:53:44 +0530

    first commit

---
+++ carapi/src/main/java/com/seetaram/poc/carapi/CarapiApplication.java
@@ -0,0 +1,13 @@
+package com.seetaram.poc.carapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarapiApplication.class, args);
+	}
+
+}
